89281e02a9424b438d8e56c4d0be4fa7131aca7b
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-06-08 18:05:35 +0200

    add de-serialized test

--- src/main/java/com/herokuapp/restfulbooker/Booking.java
+++ src/main/java/com/herokuapp/restfulbooker/Booking.java
@@ -18,64 +18,75 @@ public class Booking {
         this.additionalneeds = additionalneeds;
     }
 
+
+    public Booking() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Booking [firstname=" + firstname + ", lastname=" + lastname + ", totalprice=" + totalprice + ", depositpaid=" + depositpaid
+                + ", bookingdates=" + bookingdates + ", additionalneeds=" + additionalneeds + "]";
+    }
+
+
     public String getFirstname() {
         return firstname;
     }
+
 
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
+
     public String getLastname() {
         return lastname;
     }
+
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
+
     public int getTotalprice() {
         return totalprice;
     }
+
 
     public void setTotalprice(int totalprice) {
         this.totalprice = totalprice;
     }
 
+
     public boolean isDepositpaid() {
         return depositpaid;
     }
+
 
     public void setDepositpaid(boolean depositpaid) {
         this.depositpaid = depositpaid;
     }
 
+
     public Bookingdates getBookingdates() {
         return bookingdates;
     }
+
 
     public void setBookingdates(Bookingdates bookingdates) {
         this.bookingdates = bookingdates;
     }
 
+
     public String getAdditionalneeds() {
         return additionalneeds;
     }
+
 
     public void setAdditionalneeds(String additionalneeds) {
         this.additionalneeds = additionalneeds;
     }
 
-    @Override
-    public String toString() {
-        return "Booking{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", totalprice=" + totalprice +
-                ", depositpaid=" + depositpaid +
-                ", bookingdates=" + bookingdates +
-                ", additionalneeds='" + additionalneeds + '\'' +
-                '}';
-    }
 }
-
--- src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
+++ src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
@@ -47,43 +47,24 @@ public class CreateBookingTests extends BaseTest {
 
     @Test
     public void createBookingWithPOJOTest() {
-       // Create body using POJOs
+        // Create body using POJOs
         Bookingdates bookingdates = new Bookingdates("2024-07-01", "2024-07-15");
         Booking booking = new Booking("Nadiia", "Semenenko", 200, false, bookingdates, "Baby crib");
 
         // Get response
-        Response response = RestAssured.given(spec).contentType(ContentType.JSON).
-                body(booking).post("/booking");
+        Response response = RestAssured.given(spec).contentType(ContentType.JSON).body(booking)
+                .post("/booking");
         response.print();
+        Bookingid bookingid = response.as(Bookingid.class);
 
         // Verifications
         // Verify response 200
         Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
 
-        // Verify all fields
-        SoftAssert softAssert = new SoftAssert();
+        System.out.println("Request booking : " + booking.toString());
+        System.out.println("Response booking: " + bookingid.getBooking().toString());
 
-        String actualFirstName = response.jsonPath().getString("booking.firstname");
-        softAssert.assertEquals(actualFirstName, "Nadiia", "First name in response is not expected");
-
-        String actualLastName = response.jsonPath().getString("booking.lastname");
-        softAssert.assertEquals(actualLastName, "Semenenko", "Last name in response is not expected");
-
-        int price = response.jsonPath().getInt("booking.totalprice");
-        softAssert.assertEquals(price, 200, "Total price in response is not expected");
-
-        boolean depositpaid = response.jsonPath().getBoolean("booking.depositpaid");
-        softAssert.assertFalse(depositpaid, "Deposit paid should be false, but it's not");
-
-        String actualCheckin = response.jsonPath().getString("booking.bookingdates.checkin");
-        softAssert.assertEquals(actualCheckin, "2024-07-01", "checkin in response is not expected");
-
-        String actualCheckout = response.jsonPath().getString("booking.bookingdates.checkout");
-        softAssert.assertEquals(actualCheckout, "2024-07-15", "checkout in response is not expected");
-
-        String actualAdditionalneeds = response.jsonPath().getString("booking.additionalneeds");
-        softAssert.assertEquals(actualAdditionalneeds, "Baby crib", "Additional needs in response is not expected");
-
-        softAssert.assertAll();
+        // Verify All fields
+        Assert.assertEquals(bookingid.getBooking().toString(), booking.toString());
     }
 }
