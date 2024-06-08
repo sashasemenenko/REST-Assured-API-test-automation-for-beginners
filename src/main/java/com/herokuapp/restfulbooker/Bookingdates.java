c82edcbc858655da4dd50f7e89bc41644a264797
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-06-08 16:03:48 +0200

    add serialzation test

---
+++ src/main/java/com/herokuapp/restfulbooker/Bookingdates.java
@@ -0,0 +1,35 @@
+package com.herokuapp.restfulbooker;
+
+public class Bookingdates {
+    private String checkin;
+    private String checkout;
+
+    public Bookingdates(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    @Override
+    public String toString() {
+        return "Bookingdates{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
--- src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
+++ src/test/java/com/herokuapp/restfulbooker/CreateBookingTests.java
@@ -1,5 +1,7 @@
 package com.herokuapp.restfulbooker;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,7 +9,7 @@ import org.testng.asserts.SoftAssert;
 
 public class CreateBookingTests extends BaseTest {
 
-    @Test
+    //@Test
     public void createBookingTest() {
         Response response = createBooking();
         response.print();
@@ -27,6 +29,48 @@ public class CreateBookingTests extends BaseTest {
 
         int price = response.jsonPath().getInt("booking.totalprice");
         softAssert.assertEquals(price, 150, "Total price in response is not expected");
+
+        boolean depositpaid = response.jsonPath().getBoolean("booking.depositpaid");
+        softAssert.assertFalse(depositpaid, "Deposit paid should be false, but it's not");
+
+        String actualCheckin = response.jsonPath().getString("booking.bookingdates.checkin");
+        softAssert.assertEquals(actualCheckin, "2024-07-01", "checkin in response is not expected");
+
+        String actualCheckout = response.jsonPath().getString("booking.bookingdates.checkout");
+        softAssert.assertEquals(actualCheckout, "2024-07-15", "checkout in response is not expected");
+
+        String actualAdditionalneeds = response.jsonPath().getString("booking.additionalneeds");
+        softAssert.assertEquals(actualAdditionalneeds, "Baby crib", "Additional needs in response is not expected");
+
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void createBookingWithPOJOTest() {
+       // Create body using POJOs
+        Bookingdates bookingdates = new Bookingdates("2024-07-01", "2024-07-15");
+        Booking booking = new Booking("Nadiia", "Semenenko", 200, false, bookingdates, "Baby crib");
+
+        // Get response
+        Response response = RestAssured.given(spec).contentType(ContentType.JSON).
+                body(booking).post("/booking");
+        response.print();
+
+        // Verifications
+        // Verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
+
+        // Verify all fields
+        SoftAssert softAssert = new SoftAssert();
+
+        String actualFirstName = response.jsonPath().getString("booking.firstname");
+        softAssert.assertEquals(actualFirstName, "Nadiia", "First name in response is not expected");
+
+        String actualLastName = response.jsonPath().getString("booking.lastname");
+        softAssert.assertEquals(actualLastName, "Semenenko", "Last name in response is not expected");
+
+        int price = response.jsonPath().getInt("booking.totalprice");
+        softAssert.assertEquals(price, 200, "Total price in response is not expected");
 
         boolean depositpaid = response.jsonPath().getBoolean("booking.depositpaid");
         softAssert.assertFalse(depositpaid, "Deposit paid should be false, but it's not");
