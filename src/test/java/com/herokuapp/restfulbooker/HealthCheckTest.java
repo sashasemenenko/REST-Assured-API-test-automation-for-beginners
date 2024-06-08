771f5169f38f8bfd6e6f1d3b07dcac64b835943b
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-06-08 14:13:48 +0200

    add Headers and Cookies

--- src/test/java/com/herokuapp/restfulbooker/HealthCheckTest.java
+++ src/test/java/com/herokuapp/restfulbooker/HealthCheckTest.java
@@ -1,5 +1,11 @@
 package com.herokuapp.restfulbooker;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,5 +20,33 @@ public class HealthCheckTest extends BaseTest {
                 then().
                 assertThat().
                 statusCode(201);
+    }
+
+    @Test
+    public void headersAndCookiesTest() {
+        Header someHeader = new Header("some_name", "some_value");
+        spec.header(someHeader);
+
+        Cookie someCookie = new Cookie.Builder("some_cookie", "some_value").build();
+        spec.cookie(someCookie);
+
+        Response response = RestAssured.given(spec).
+                cookie("Test cookie name", "Test cookie value").
+                header("Test header name", "Test header value").log().all().get("/ping");
+
+        // Get headers
+        Headers headers = response.getHeaders();
+        System.out.println("Headers: " + headers);
+
+        Header serverHeader1 = headers.get("Server");
+        System.out.println(serverHeader1.getName() + ": " + serverHeader1.getValue());
+
+        String serverHeader2 = response.getHeader("Server");
+        System.out.println("Server: " + serverHeader2);
+
+        // Get cookies
+        Cookies cookies = response.getDetailedCookies();
+        System.out.println("Cookies: " + cookies);
+
     }
 }
