e0aef2dde88a7b4a637cdc9a2f1cbdc5f95e839a
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-06-08 13:50:00 +0200

    add query parameters

--- src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingIdsTests.java
@@ -1,9 +1,7 @@
 package com.herokuapp.restfulbooker;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,9 +11,24 @@ public class GetBookingIdsTests extends BaseTest {
 
     @Test
     public void getBookingIdsWithoutFilterTest() {
-        RequestSpecification spec = new RequestSpecBuilder().
-                setBaseUri("https://restful-booker.herokuapp.com").
-                build();
+        // Get response with booking ids
+        Response response = RestAssured.given(spec).get("/booking");
+        response.print();
+
+        // Verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
+
+        // Verify at least 1 booking id in response
+
+        List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingIds.isEmpty(), "List of bookingIds is empty, but it shouldn't be");
+    }
+
+    @Test
+    public void getBooingIsWithFilterTest() {
+        // add query parameter to spec
+        spec.queryParam("firstname", "Sally");
+        spec.queryParam("lastname", "Brown");
 
         // Get response with booking ids
         Response response = RestAssured.given(spec).get("/booking");
